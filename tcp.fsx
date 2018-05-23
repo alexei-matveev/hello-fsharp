d4b2c0c7587a74fb745f79ead0fcdb37ddc42b9a
Alexei Matveev <alexei.matveev@gmail.com>
2018-05-23 21:08:45 +0200

    AM: these are disposable

--- tcp.fsx
+++ tcp.fsx
@@ -9,6 +9,8 @@ let write (s: NetworkStream) buf =
     s.Write(buf, 0, buf.Length)
 
 let makeStream host port =
+    // Dont s/let/use/ here! Disposing of of client
+    // also closes the stream:
     let client = new TcpClient(host, port)
     client.GetStream()
 
@@ -16,10 +18,9 @@ let getString (b: byte[]) = System.Text.Encoding.UTF8.GetString b
 let getBytes (s:string) = System.Text.Encoding.UTF8.GetBytes s
 
 let response: byte[] =
-  let s = makeStream "google.com" 80
+  use s = makeStream "google.com" 80
   write s (getBytes "GET / HTTP/1.1\r\n\r\n")
   let res = read s 256
-  s.Close()
   res
 
 printfn "%s" (getString response)
