2e8bfaa0dff102244bf64b272c7dca1abc4da4f7
Alexei Matveev <alexei.matveev@gmail.com>
2018-05-23 20:40:02 +0200

    AM: dont thread stream

--- tcp.fsx
+++ tcp.fsx
@@ -4,15 +4,17 @@ open System.Net.Sockets
 
 type stream = NetworkStream
 let curry g b n = g(b,0,n) |> ignore; b
-let read  n (s : stream) = curry s.Read (Array.zeroCreate n) n,s
-let write b (s : stream) = curry s.Write b b.Length; s
+
+let read  n (s : stream) = curry s.Read (Array.zeroCreate n) n
+
+let write b (s : stream) = curry s.Write b b.Length
 
 let connect host port = TcpClient(host, port).GetStream()
 
 let response: byte[] =
   let s = connect "google.com" 80
   write "GET / HTTP/1.1\r\n\r\n"B s
-  let res, _ = read 256 s
+  let res = read 256 s
   s.Close()
   res
 
