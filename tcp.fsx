7f2af14be8daf64ba1f4f6a0085ddfea995fd6b6
Alexei Matveev <alexei.matveev@gmail.com>
2018-05-23 20:38:43 +0200

    AM: close is unused

--- tcp.fsx
+++ tcp.fsx
@@ -6,7 +6,7 @@ type stream = NetworkStream
 let curry g b n = g(b,0,n) |> ignore; b
 let read  n (s : stream) = curry s.Read (Array.zeroCreate n) n,s
 let write b (s : stream) = curry s.Write b b.Length; s
-let close (b,(s : stream)) = s.Close(); b
+
 let connect host port = TcpClient(host, port).GetStream()
 
 let response: byte[] =
