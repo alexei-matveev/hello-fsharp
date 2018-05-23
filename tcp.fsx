5633b5bfcd4fad500448442323f8d4538dd5a5ad
Alexei Matveev <alexei.matveev@gmail.com>
2018-05-23 20:37:50 +0200

    AM: get rid of pipeline

--- tcp.fsx
+++ tcp.fsx
@@ -7,15 +7,15 @@ let curry g b n = g(b,0,n) |> ignore; b
 let read  n (s : stream) = curry s.Read (Array.zeroCreate n) n,s
 let write b (s : stream) = curry s.Write b b.Length; s
 let close (b,(s : stream)) = s.Close(); b
-let connect host port = TcpClient(host,port).GetStream()
+let connect host port = TcpClient(host, port).GetStream()
 
-let response : byte[] = 
-  connect "google.com" 80
-  |> write "GET / HTTP/1.1\r\n\r\n"B
-  |> read 256
-  |> close
+let response: byte[] =
+  let s = connect "google.com" 80
+  write "GET / HTTP/1.1\r\n\r\n"B s
+  let res, _ = read 256 s
+  s.Close()
+  res
 
-printfn "%s" "hi there"
 printfn "%s" (System.Text.Encoding.ASCII.GetString response)
 
 
