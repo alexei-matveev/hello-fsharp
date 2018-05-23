bfadecaa1dc4d05700d2656ec2a5ea0e4284ab41
Alexei Matveev <alexei.matveev@gmail.com>
2018-05-23 21:11:32 +0200

    AM: inline makeStream for use

--- tcp.fsx
+++ tcp.fsx
@@ -8,17 +8,12 @@ let read (s: NetworkStream) n =
 let write (s: NetworkStream) buf =
     s.Write(buf, 0, buf.Length)
 
-let makeStream host port =
-    // Dont s/let/use/ here! Disposing of of client
-    // also closes the stream:
-    let client = new TcpClient(host, port)
-    client.GetStream()
-
 let getString (b: byte[]) = System.Text.Encoding.UTF8.GetString b
 let getBytes (s:string) = System.Text.Encoding.UTF8.GetBytes s
 
 let response: byte[] =
-  use s = makeStream "google.com" 80
+  use client = new TcpClient("google.com", 80)
+  use s = client.GetStream()
   write s (getBytes "GET / HTTP/1.1\r\n\r\n")
   let res = read s 256
   res
