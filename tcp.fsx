889deb9526b477167b87b7c8570155edc16edb4e
Alexei Matveev <alexei.matveev@gmail.com>
2018-05-23 21:20:06 +0200

    AM: indet and formatting

--- tcp.fsx
+++ tcp.fsx
@@ -8,15 +8,18 @@ let read (s: NetworkStream) n =
 let write (s: NetworkStream) buf =
     s.Write(buf, 0, buf.Length)
 
-let getString (b: byte[]) = System.Text.Encoding.UTF8.GetString b
-let getBytes (s: string) = System.Text.Encoding.UTF8.GetBytes s
+let getString (b: byte[]) =
+    System.Text.Encoding.UTF8.GetString b
+
+let getBytes (s: string) =
+    System.Text.Encoding.UTF8.GetBytes s
 
 let ping host port request =
-  use client = new TcpClient(host, port)
-  use stream = client.GetStream()
-  write stream (getBytes request)
-  let res = read stream 256
-  getString res
+    use client = new TcpClient(host, port)
+    use stream = client.GetStream()
+    write stream (getBytes request)
+    let res = read stream 256
+    getString res
 
 let response = ping "google.com" 80 "GET / HTTP/1.1\r\n\r\n" in
     printfn "%s" response
